a31414b34f60dfc35502ae99e3cf0d4c424d9ac1
CassioLimaP <cachedosistema@gmail.com>
2025-07-29 19:45:28 -0300

    tentei continuar com a logica mas estava muito malfeita

--- codigo_de_cesar.py
+++ codigo_de_cesar.py
@@ -1,4 +1,4 @@
-#implementar a cifra de cesar.
+#implementar a cifra de cesar. @cassioLimaP
 #string movendo cada letra para um certo numero de passos no alfabeto
 #o numero de passos é dado por uma chave. letras com acentos, espacos e pontuacao permanecem iguais
 #"abcd" com 1 chave = "bcde"
@@ -16,31 +16,63 @@
     #4.2- ver numero referente a letra do alfabeto e trocar de acordo a chave
     #4.3- isso letra a letra da palavra a ser codificada
 #5- saida esperada-> "a palavra "olá tudo bem" com a chave -2 fica "mjá rsbom zck". "
-def codificacao_cesar(entrada_do_codigo,chave):
-    maiusculas = "abcdefghijklmnopqrstuvwxyz"
-    minusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    codigo_pronto = " "
+#ord('A') retorna 65
+#ord('Z') retorna 90
+#ord('a') retorna 97
+#ord('z') retorna 122
+#chr(97) retorna 'a'
+def codificacao_cesar(entrada_do_codigo, chave):
+    minusculas = "abcdefghijklmnopqrstuvwxyz"
+    maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    codigo_pronto = ""
     controle_de_posicoes = 0
-    for i in entrada_do_codigo[]:
-        for j in range(1,26):
-            if entrada_do_codigo[i] == maiusculas[j] :
-                #codifica com maiusculas
-                #1- valor de entrada compara com maiusculas
-                #2- se verdade
-                    #2.1- letra da entrada recebe a letra da posicao posicao+chave de maiusculas
-                    #2.2- fazer as letras voltarem ao inicio se extrapolarem o valor total de 26
-                #fim
-                if maiusculas[j+chave]>25:
+    for i in entrada_do_codigo:
+        for j in range(0,25):
+            if entrada_do_codigo[i] == maiusculas[j] :#codifica com maiusculas ASCII(65 ate 90)
+                
+                if ord(maiusculas[j+chave]) > ord('Z'):
                     controle_de_posicoes = ((j+chave)%26)
-                    codigo_pronto[i] = maiusculas[0+controle_de_posicoes]
-                    
-            elif entrada_do_codigo[i] == minusculas[j] :
-                #codifica com minusculas
-        #fim do for de dentro
-    #fim do for de fora 
-return codigo_pronto
+                    codigo_pronto = codigo_pronto+maiusculas[controle_de_posicoes]
+                    break
+                
+                elif ord(maiusculas[j+chave]) < ord('A'):
+                    controle_de_posicoes = ((j+chave)%26)
+                    codigo_pronto = codigo_pronto+maiusculas[25+controle_de_posicoes]
+                    break
+                
+                else:
+                    controle_de_posicoes = (ord(entrada_do_codigo[i]) - ord('A'))
+                    codigo_pronto = codigo_pronto+maiusculas[controle_de_posicoes+chave]
+                    break
 
-entrada_do_codigo = "olá tudo bem?" 
+            elif entrada_do_codigo[i] == minusculas[j]:#codifica com minusculas ASCII(97 ate 122)
+
+                if ord(maiusculas[j+chave]) > ord('Z'):
+                    controle_de_posicoes = ((j+chave)%26)
+                    codigo_pronto = codigo_pronto+maiusculas[controle_de_posicoes]
+                    break
+                
+                elif ord(maiusculas[j+chave]) < ord('A'):
+                    controle_de_posicoes = ((j+chave)%26)
+                    codigo_pronto = codigo_pronto+maiusculas[25+controle_de_posicoes]
+                    break
+                
+                else:
+                    controle_de_posicoes = (ord(entrada_do_codigo[i]) - ord('A'))
+                    codigo_pronto = codigo_pronto+maiusculas[controle_de_posicoes+chave]
+                    break
+                    
+            else:
+                codigo_pronto = codigo_pronto+entrada_do_codigo[i]
+                break
+        #fim do for de dentro
+        print(codigo_pronto)
+    #fim do for de fora
+    print("codigo original:",entrada_do_codigo,"com chave:",chave)
+    print("resultado:",codigo_pronto)
+    #return codigo_pronto
+entrada_do_codigo = "olá tudo bem?"
 chave=-2
-print(maiusculas[4])
+codificacao_cesar(entrada_do_codigo, chave)
+#print(maiusculas[4])
 
