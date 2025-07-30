0181088bc6112a7485fd8fbf5a9bacceb210839c
CassioLimaP <cachedosistema@gmail.com>
2025-07-30 18:47:31 -0300

    troquei a branch main pra melhor entendimento. esta é a nova main

---
+++ exercicios simples/exemplificacao_util.py
@@ -0,0 +1,13 @@
+var_0 = int(input("digite a idade: "))
+#direciona a variavel para ser inteiro
+var_1 = input("digite a idade")
+#esta acima armazena o valor numerico como string
+
+nome = input("digite seu nome")
+#um nome qualquer
+tamanho_nome = len(nome)
+#armazena o tamanho do nome
+
+
+
+
