a93db0a89f8f5ae288c736f722eba2a1eb55aa6d
CassioLimaP <cachedosistema@gmail.com>
2025-07-30 18:23:45 -0300

    adicionando mais alguns codigos para exemplos

---
+++ exercicios simples/desafio_entrevista.py
@@ -0,0 +1,11 @@
+#pede seu nome e idade
+#da oi para voce
+#conta quantas letras seu nome possui
+#fala quantos anos voce tera em 5 anos
+
+nome = input("digite seu nome\n")
+idade = input("digite sua idade")
+
+print("ola! ")
+print(f"voce tera:{int(idade)+5} em 5 anos")
+print(f"seu nome tem:{len(nome)} letras")
