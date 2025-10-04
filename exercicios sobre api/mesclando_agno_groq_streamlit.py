9978a84b5a6f79d4287fa52f247c755c4443481e
CassioLimaP <cachedosistema@gmail.com>
2025-10-04 13:02:25 -0300

    agente funcionando. falta implementar o streamlit

--- exercicios sobre api/mesclando_agno_groq_streamlit.py
+++ exercicios sobre api/mesclando_agno_groq_streamlit.py
@@ -2,42 +2,29 @@
 # vou tentar criar aqui em partes, a juncao de agno basico com o groq
 # groq para escolher as LLM e o agno para criar os agentes.
 # posteriormente vou tentar fazer isso rodar com o streamlit
-#
-import os
 from dotenv import load_dotenv
-from openai import OpenAI
+from agno.models.groq import Groq
 from agno.agent import Agent
-
 load_dotenv()
-#o esquema aqui é que o agno funciona com o OpenAI entao vou usar ele 
-#so que com a api e endereço do groq 
-groq_client = OpenAI(
-    api_key=os.environ.get("GROQ_API_KEY"),
-    base_url="https://api.groq.com/openai/v1/chat",
-)
-
-#escolhendo um modelo  
-MODELO_GROQ = "llama3-8b-8192"
-
+groq_model= Groq(id="llama-3.1-8b-instant")
 #criando o agente 
+agente_veloz = Agent(
+    #passamos nosso cliente personalizado
+    model=groq_model,
+    name="Agente jarvis",
+    description={
+        '''
+        Você é um assistente prestativo. 
+        voce passa informações de forma clara e com atencao ao que foi pedido.
+        .
+        '''
+    }
+)
 try:
-    agente_veloz = Agent(
-        #passamos nosso cliente personalizado
-        client=groq_client, 
-        #Especifique o nome do modelo que o Groq deve usar
-        model=MODELO_GROQ,
-        # O resto da configuração do seu agente continua a mesma
-        name="Agente jarvis",
-        system_prompt="Você é um assistente prestativo e serio. passa as informações de forma clara e objetiva."
-    )
     flag = True 
     while flag:
         prompt_usuario = input("usuario: ")
-        print(f"\nusuario: {prompt_usuario}")
-        resposta = agente_veloz.run(prompt_usuario)
-        print("\n--- Resposta do Agente (via Groq) ---")
-        print(resposta)
-        print("--------------------------------------")
+        agente_veloz.print_response(prompt_usuario)
 except Exception as e:
     print(f"\nOcorreu um erro: {e}")
     print("Verifique se sua API Key do Groq está correta e se o modelo '{MODELO_GROQ}' é válido.")
